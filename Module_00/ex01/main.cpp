21a53fe196f13c8badadaa1ffa6a59ad59a7ff0c
Hiroaki-K4 <49no19@gmail.com>
2022-02-23 22:58:35 +0900

    #3 Update main.cpp

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "hello" << std::endl;
+	(void)argv;
+	if (argc == 1)
+	{
+		std::string str;
+		std::cout << "My Awesome PhoneBook" << std::endl;
+		while (true)
+		{
+			std::cin >> str;
+			if (str == "EXIT")
+				break;
+			else if (str == "ADD")
+			{
+				std::cout << "Please enter first name, last name, nickname, phone number,darkest secret." << std::endl;
+				std::string contact;
+				std::cin >> contact;
+				std::cout << contact << std::endl;
+			}
+		}
+	}
 	return (0);
 }
