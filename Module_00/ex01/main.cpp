71e1bece74b67621153c3fed3691376822fcc420
Hiroaki-K4 <49no19@gmail.com>
2022-03-03 22:24:41 +0900

    #3 Add ADD process

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -14,10 +14,23 @@ int main(int argc, char *argv[])
 				break;
 			else if (str == "ADD")
 			{
-				std::cout << "Please enter first name, last name, nickname, phone number,darkest secret." << std::endl;
-				std::string contact;
-				std::cin >> contact;
-				std::cout << contact << std::endl;
+				std::cout << "Please enter first name, last name, nickname, phone number, darkest secret." << std::endl;
+				std::cout << "Please enter first name." << std::endl;
+				std::string first_name;
+				std::cin >> first_name;
+				std::cout << "Please enter last name." << std::endl;
+				std::string last_name;
+				std::cin >> last_name;
+				std::cout << "Please enter nickname." << std::endl;
+				std::string nick_name;
+				std::cin >> nick_name;
+				std::cout << "Please enter phone number." << std::endl;
+				std::string phone_number;
+				std::cin >> phone_number;
+				std::cout << "Please enter darkest secret." << std::endl;
+				std::string darkest_secret;
+				std::cin >> darkest_secret;
+				std::cout << first_name << " " << last_name << " " << nick_name << " " << phone_number << " " << darkest_secret << std::endl;
 			}
 		}
 	}
