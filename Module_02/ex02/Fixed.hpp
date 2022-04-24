373413127fbd06eb5bb93d186876dffac3fc472e
Hiroaki-K4 <49no19@gmail.com>
2022-04-24 22:15:55 +0900

    #23 Make Module02_ex02

---
+++ Module_02/ex02/Fixed.hpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/24 14:08:51 by hkubo             #+#    #+#             */
+/*   Updated: 2022/04/24 22:08:56 by hkubo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int fractional_bits_num = 8;
+
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &obj);
+        Fixed(const int num);
+        Fixed(const float f_num);
+        bool operator>(const Fixed &obj);
+        bool operator<(const Fixed &obj);
+        bool operator>=(const Fixed &obj);
+        bool operator<=(const Fixed &obj);
+        bool operator==(const Fixed &obj);
+        bool operator!=(const Fixed &obj);
+        Fixed operator+(const Fixed &obj);
+        Fixed operator-(const Fixed &obj);
+        Fixed operator*(const Fixed &obj);
+        Fixed operator/(const Fixed &obj);
+        Fixed &operator=(const Fixed &obj);
+        Fixed &operator++();
+        Fixed &operator--();
+        Fixed operator++(int notused);
+        Fixed operator--(int notused);
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt(void) const;
+        static const Fixed &max(const Fixed &obj1, const Fixed &obj2);
+        static const Fixed &min(const Fixed &obj1, const Fixed &obj2);
+        static Fixed &max(Fixed &obj1, Fixed &obj2);
+        static Fixed &min(Fixed &obj1, Fixed &obj2);
+};
+
+std::ostream &operator<<(std::ostream& os, const Fixed &obj);
+
+#endif
