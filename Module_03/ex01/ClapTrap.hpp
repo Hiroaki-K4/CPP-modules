aa002489406c966cfadd72d094880b5df7318eda
Hiroaki-K4 <49no19@gmail.com>
2022-04-25 22:28:40 +0900

    #27 Make Module03_ex01

---
+++ Module_03/ex01/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 20:46:45 by hkubo             #+#    #+#             */
+/*   Updated: 2022/04/25 22:13:44 by hkubo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string name;
+        unsigned int hit_points;
+        unsigned int energy_points;
+        unsigned int attack_damage;
+
+    public:
+        ClapTrap();
+        ClapTrap(const std::string &name);
+        ClapTrap(const ClapTrap &obj);
+        ~ClapTrap();
+        ClapTrap &operator=(const ClapTrap &obj);
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
+
+#endif
