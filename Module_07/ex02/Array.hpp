e4535747cbde8e4b9f61d97e68fe06b46bf12a3e
Hiroaki-K4 <49no19@gmail.com>
2022-05-30 21:57:14 +0900

    #55 Update Module07_ex02

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 14:34:20 by hkubo             #+#    #+#             */
-/*   Updated: 2022/05/29 17:02:58 by hkubo            ###   ########.fr       */
+/*   Updated: 2022/05/30 21:48:48 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ template <typename T> class Array
     private:
         T *array;
         unsigned int array_size;
+
     public:
         Array()
         {
@@ -31,17 +32,23 @@ template <typename T> class Array
         };
         Array(const Array &obj)
         {
-            *this = obj;
+            this->array_size = obj.size();
+            this->array = new T[obj.size()];
+            for (unsigned int i = 0; i < obj.size(); i++)
+                this->array[i] = obj[i];
         };
         Array &operator=(const Array &obj)
         {
-            this->array = obj.array;
-            this->array_size = obj.array_size;
+            this->~Array();
+            this->array_size = obj.size();
+            this->array = new T[obj.size()];
+            for (unsigned int i = 0; i < obj.size(); i++)
+                this->array[i] = obj[i];
             return (*this);
         };
-        T &operator[](const unsigned int n)
+        T &operator[](int n) const
         {
-            if (n >= array_size)
+            if (n < 0 || n >= static_cast<int>(this->size()))
                 throw std::exception();
             return (array[n]);
         };
@@ -49,9 +56,9 @@ template <typename T> class Array
         {
             delete[] this->array;
         };
-        unsigned int size()
+        unsigned int size() const
         {
-            return this->array_size;
+            return (this->array_size);
         };
         T getArray()
         {
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 21:36:47 by hkubo             #+#    #+#             */
-/*   Updated: 2022/05/29 17:29:00 by hkubo            ###   ########.fr       */
+/*   Updated: 2022/05/30 21:55:39 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 
 int main(void)
 {
-    Array<std::string> test1(3);
+    std::cout << "~~~No parameter test~~~" << std::endl;
+    Array<std::string> test;
+    try
+    {
+        for (int i = 0; i < static_cast<int>(test.size()); i++)
+            std::cout << test[i] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
     std::cout << "~~~String test~~~" << std::endl;
+    Array<std::string> test1(3);
     try
     {
         test1[0] = "hello1";
         test1[1] = "hello2";
         test1[2] = "hello3";
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < static_cast<int>(test1.size()) + 1; i++)
             std::cout << test1[i] << std::endl;
     }
     catch (std::exception &e)
@@ -30,16 +43,43 @@ int main(void)
         std::cout << e.what() << std::endl;
     }
 
-    Array<int *> test2(3);
     std::cout << std::endl;
     std::cout << "~~~Int test~~~" << std::endl;
+    Array<int> test2(3);
     try
     {
-        test1[0] = 1;
-        test1[1] = 2;
-        test1[2] = 3;
-        for (int i = 0; i < 4; i++)
-            std::cout << test1[i] << std::endl;
+        test2[0] = 1;
+        test2[1] = 2;
+        test2[2] = 3;
+        for (int i = 0; i < static_cast<int>(test2.size()); i++)
+            std::cout << test2[i] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "~~~Assignment operator test~~~" << std::endl;
+    Array<std::string> test3;
+    test3 = test1;
+    try
+    {
+        for (int i = 0; i < static_cast<int>(test3.size()); i++)
+            std::cout << test3[i] << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "~~~Copy test~~~" << std::endl;
+    Array<int> test4(test2);
+    try
+    {
+        for (int i = 0; i < static_cast<int>(test4.size()); i++)
+            std::cout << test4[i] << std::endl;
     }
     catch (std::exception &e)
     {
