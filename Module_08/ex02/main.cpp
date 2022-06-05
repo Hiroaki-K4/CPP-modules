03755933682ea5796c3ae181653fa7abd5f07adc
Hiroaki-K4 <49no19@gmail.com>
2022-06-05 20:55:00 +0900

    #61 Add std::list test

--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -6,17 +6,18 @@
 /*   By: hkubo <hkubo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 21:57:48 by hkubo             #+#    #+#             */
-/*   Updated: 2022/06/05 18:38:20 by hkubo            ###   ########.fr       */
+/*   Updated: 2022/06/05 20:52:25 by hkubo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <list>
 #include <stack>
 #include "MutantStack.hpp"
 
 int main(void)
 {
-    std::cout << "~~~Test MutantStack~~~" << std::endl;
+    std::cout << "~~~MutantStack test~~~" << std::endl;
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
@@ -42,6 +43,33 @@ int main(void)
         ++it;
     }
     std::stack<int> s(mstack);
+    std::cout << std::endl;
+
+    std::cout << "~~~List test~~~" << std::endl;
+    std::list<int> test;
+    test.push_back(5);
+    test.push_back(17);
+    std::cout << "Size: " << test.size() << " Top: " << test.back() << std::endl;
+    std::cout << "Delete top" << std::endl;
+    test.pop_back();
+    std::cout << "Size: " << test.size() << " Top: " << test.back() << std::endl;
+    std::cout << "Delete top" << std::endl;
+    test.pop_back();
+    std::cout << "Empty: " << test.empty() << std::endl;
+
+    test.push_back(3);
+    test.push_back(5);
+    test.push_back(737);
+    test.push_back(0);
+    std::list<int>::iterator list_it = test.begin();
+    std::list<int>::iterator list_ite = test.end();
+    ++list_it;
+    --list_it;
+    while (list_it != list_ite)
+    {
+        std::cout << *list_it << std::endl;
+        ++list_it;
+    }
 
     return (0);
 }
